4ac7254748a98ec4d7d2990f7239903eef9dc3c1
bruano95 <bruano@uw.edu>
2023-05-21 21:40:58 -0700

    updated javascripts for database, install to get to work.

--- Develop/client/src/js/install.js
+++ Develop/client/src/js/install.js
@@ -3,10 +3,8 @@ const butInstall = document.getElementById('buttonInstall');
 // Logic for installing the PWA
 // TODO: Add an event handler to the `beforeinstallprompt` event
 window.addEventListener('beforeinstallprompt', (event) => {
-        // Store the triggered events
         window.deferredPrompt = event;
 
-        // Remove the hidden class from the button.
         butInstall.classList.toggle('hidden', false);
 });
 
@@ -19,10 +17,8 @@ butInstall.addEventListener('click', async () => {
      return;
     }
   
-    // Show prompt
     promptEvent.prompt();
-    
-    // Reset the deferred prompt variable, it can only be used once.
+
     window.deferredPrompt = null;
     
     butInstall.classList.toggle('hidden', true);
